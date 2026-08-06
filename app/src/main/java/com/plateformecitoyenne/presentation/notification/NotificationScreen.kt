d95993cc9b8a40c3f5650248d0b8bea997edd7ca
Faty Malo Samb <fatymalosamb@gmail.com>
2026-08-06 23:55:04 +0000

    Correction NotificationScreen et organisation des packages

--- app/src/main/java/com/plateformecitoyenne/presentation/notification/NotificationScreen.kt
+++ app/src/main/java/com/plateformecitoyenne/presentation/notification/NotificationScreen.kt
@@ -1,15 +1,27 @@
 package com.plateformecitoyenne.presentation.notification
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.plateformecitoyenne.model.Notification
 import com.plateformecitoyenne.network.RetrofitClient
-
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 @Composable
 fun NotificationScreen(
@@ -63,7 +75,6 @@ fun NotificationScreen(
     ) {
 
 
-
         Button(
             onClick = {
                 onRetour()
@@ -95,7 +106,7 @@ fun NotificationScreen(
 
 
 
-        if(message.isNotEmpty()) {
+        if (message.isNotEmpty()) {
 
             Text(message)
 
@@ -147,7 +158,7 @@ fun NotificationScreen(
 
                     Text(
                         text =
-                            if(notification.lue)
+                            if (notification.lue)
                                 "Lu"
                             else
                                 "Nouveau"
